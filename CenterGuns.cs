82ad03c0a1654adbf77962b06e542a773b9834d6
skaju0 <skaju.kontakt@gmail.com>
2026-08-21 17:04:27 +0200

    Update CenterGuns.cs

--- CenterGuns.cs
+++ CenterGuns.cs
@@ -8,7 +8,7 @@ namespace CenterGuns;
 public class CenterGuns : BasePlugin
 {
     public override string ModuleName => "Center Guns Menu";
-    public override string ModuleVersion => "1.0.1";
+    public override string ModuleVersion => "1.0.2";
     public override string ModuleAuthor => "1337HUB";
 
     private const string Tag = " \u000B[1337HUB.PL]\u0001";
@@ -40,21 +40,21 @@ public class CenterGuns : BasePlugin
 
     private void OpenGunMenu(CCSPlayerController player)
     {
-        var menu = new CenterHtmlMenu("Wybór Broni [1337HUB]", this);
+        // Użycie ChatMenu z opcją cyfrową (wybór bezpośrednio klawiszami 1-9)
+        var menu = new ChatMenu("Wybór Broni [1337HUB]");
 
         menu.AddMenuOption("AK-47 + Deagle", (p, opt) => GiveWeapons(p, "weapon_ak47", "weapon_deagle"));
         menu.AddMenuOption("M4A1-S + USP", (p, opt) => GiveWeapons(p, "weapon_m4a1_silencer", "weapon_usp_silencer"));
         menu.AddMenuOption("M4A4 + USP", (p, opt) => GiveWeapons(p, "weapon_m4a1", "weapon_usp_silencer"));
         menu.AddMenuOption("AWP + Deagle", (p, opt) => GiveWeapons(p, "weapon_awp", "weapon_deagle"));
 
-        MenuManager.OpenCenterHtmlMenu(this, player, menu);
+        MenuManager.OpenChatMenu(player, menu);
     }
 
     private void GiveWeapons(CCSPlayerController player, string primary, string secondary)
     {
         if (player == null || player.PlayerPawn.Value == null) return;
 
-        // Wymuszenie natychmiastowego zamknięcia menu po dokonaniu wyboru
         MenuManager.CloseActiveMenu(player);
 
         player.RemoveWeapons();
