47e7c37062007ee141300e41ad37946f8f0bdcbb
skaju0 <skaju.kontakt@gmail.com>
2026-08-21 17:23:40 +0200

    Update CenterGuns.cs

--- CenterGuns.cs
+++ CenterGuns.cs
@@ -8,26 +8,23 @@ namespace CenterGuns;
 public class CenterGuns : BasePlugin
 {
     public override string ModuleName => "Center Guns Menu";
-    public override string ModuleVersion => "1.0.6";
+    public override string ModuleVersion => "1.0.8";
     public override string ModuleAuthor => "1337HUB";
 
     private const string Tag = " \u000B[1337HUB.PL]\u0001";
+    private readonly Dictionary<ulong, string> _playerSecondary = new();
 
     public override void Load(bool hotReload)
     {
         AddCommand("css_guns", "Otwiera menu broni", OnGunsCommand);
         AddCommand("css_gun", "Otwiera menu broni", OnGunsCommand);
 
-        // Komendy szybkiego wyboru (!1 - !9)
-        AddCommand("css_1", "Zestaw 1", (p, info) => GiveWeapons(p, "weapon_ak47", "weapon_deagle"));
-        AddCommand("css_2", "Zestaw 2", (p, info) => GiveWeapons(p, "weapon_m4a1_silencer", "weapon_deagle"));
-        AddCommand("css_3", "Zestaw 3", (p, info) => GiveWeapons(p, "weapon_m4a1", "weapon_deagle"));
-        AddCommand("css_4", "Zestaw 4", (p, info) => GiveWeapons(p, "weapon_awp", "weapon_deagle"));
-        AddCommand("css_5", "Zestaw 5", (p, info) => GiveWeapons(p, "weapon_ak47", "weapon_revolver"));
-        AddCommand("css_6", "Zestaw 6", (p, info) => GiveWeapons(p, "weapon_m4a1_silencer", "weapon_revolver"));
-        AddCommand("css_7", "Zestaw 7", (p, info) => GiveWeapons(p, "weapon_m4a1", "weapon_revolver"));
-        AddCommand("css_8", "Zestaw 8", (p, info) => GiveWeapons(p, "weapon_awp", "weapon_revolver"));
-        AddCommand("css_9", "Zestaw 9", (p, info) => GiveWeapons(p, "weapon_ssg08", "weapon_deagle"));
+        // Komendy szybkiego wyboru z czatu: !1, !2, !3, !4 oraz !r
+        AddCommand("css_1", "Wybór AK", (p, info) => GiveKit(p, "weapon_ak47"));
+        AddCommand("css_2", "Wybór M4A1-S", (p, info) => GiveKit(p, "weapon_m4a1_silencer"));
+        AddCommand("css_3", "Wybór M4A4", (p, info) => GiveKit(p, "weapon_m4a1"));
+        AddCommand("css_4", "Wybór AWP", (p, info) => GiveKit(p, "weapon_awp"));
+        AddCommand("css_r", "Przełącz pistolet", OnTogglePistolCommand);
     }
 
     [GameEventHandler]
@@ -43,39 +40,53 @@ public class CenterGuns : BasePlugin
         return HookResult.Continue;
     }
 
-    private void OnGunsCommand(CCSPlayerController? player, CounterStrikeSharp.API.Modules.Commands.CommandInfo info)
+    private void OnGunsCommand(CCSPlayerController? player, CommandInfo info)
     {
         if (player == null || !player.IsValid) return;
         OpenGunMenu(player);
     }
 
+    private void OnTogglePistolCommand(CCSPlayerController? player, CommandInfo info)
+    {
+        if (player == null || !player.IsValid) return;
+
+        ulong steamId = player.SteamID;
+        string currentPistol = _playerSecondary.GetValueOrDefault(steamId, "weapon_deagle");
+        string newPistol = currentPistol == "weapon_deagle" ? "weapon_revolver" : "weapon_deagle";
+
+        _playerSecondary[steamId] = newPistol;
+
+        string pistolName = newPistol == "weapon_revolver" ? "REWOLWER" : "DEAGLE";
+        player.PrintToChat($"{Tag} Zmieniono pistolet na: \u0006{pistolName}\u0001!");
+
+        // Odświeżenie otwartego menu po przełączeniu pistoletu
+        OpenGunMenu(player);
+    }
+
     private void OpenGunMenu(CCSPlayerController player)
     {
-        // Wyłączenie automatycznego stronicowania CS# - budujemy własny layout HTML
-        string content = "<b><span style='color: #ff1e1e;'>[1337HUB.PL]</span> <span style='color: #ffffff;'>WYBIERZ BROŃ</span></b><br>" +
-                         "<span style='color: #70b03a;'>!1</span> AK + DEAGLE<br>" +
-                         "<span style='color: #70b03a;'>!2</span> M4A1-S + DEAGLE<br>" +
-                         "<span style='color: #70b03a;'>!3</span> M4A4 + DEAGLE<br>" +
-                         "<span style='color: #70b03a;'>!4</span> AWP + DEAGLE<br>" +
-                         "<span style='color: #70b03a;'>!5</span> AK + REWOLWER<br>" +
-                         "<span style='color: #70b03a;'>!6</span> M4A1-S + REWOLWER<br>" +
-                         "<span style='color: #70b03a;'>!7</span> M4A4 + REWOLWER<br>" +
-                         "<span style='color: #70b03a;'>!8</span> AWP + REWOLWER<br>" +
-                         "<span style='color: #70b03a;'>!9</span> SCOUT + DEAGLE";
+        string pistolName = _playerSecondary.GetValueOrDefault(player.SteamID, "weapon_deagle") == "weapon_revolver" ? "REWOLWER" : "DEAGLE";
+
+        string content = "<b><span style='color: #ff1e1e;'>[1337HUB.PL]</span> <span style='color: #ffffff;'>WYBIERZ BROŃ</span></b><br><br>" +
+                         "<span style='color: #70b03a;'>!1</span> AK + " + pistolName + "<br>" +
+                         "<span style='color: #70b03a;'>!2</span> M4A1-S + " + pistolName + "<br>" +
+                         "<span style='color: #70b03a;'>!3</span> M4A4 + " + pistolName + "<br>" +
+                         "<span style='color: #70b03a;'>!4</span> AWP + " + pistolName + "<br><br>" +
+                         "<span style='color: #e6a100;'>!r</span> ZAMIEŃ DEAGLE NA REWOLWER I NA ODWRÓT";
 
         var menu = new CenterHtmlMenu(content, this);
-
-        // Dodajemy puste wywołania, by ramka przetworzyła natywne zbieranie danych
         menu.AddMenuOption("", (p, opt) => { });
 
         MenuManager.OpenCenterHtmlMenu(this, player, menu);
     }
 
-    private void GiveWeapons(CCSPlayerController? player, string primary, string secondary)
+    private void GiveKit(CCSPlayerController? player, string primary)
     {
         if (player == null || !player.IsValid || player.PlayerPawn.Value == null) return;
 
         MenuManager.CloseActiveMenu(player);
+
+        string secondary = _playerSecondary.GetValueOrDefault(player.SteamID, "weapon_deagle");
 
         player.RemoveWeapons();
         player.GiveNamedItem(primary);
